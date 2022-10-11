d96512737cb410871d61304f3ee78d021adc27da
LisnychaNastia <94173940+LisnychaNastia@users.noreply.github.com>
2022-10-11 20:42:22 +0300

    init commit

---
+++ Assets/Namespaces/SomeOtherClass.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SampleNamespace
+{
+    public class SampleNamespace : MonoBehaviour
+    {
+
+        void Start()
+        {
+            SomeClass myClass = new SomeClass();
+        }
+    }
+}
